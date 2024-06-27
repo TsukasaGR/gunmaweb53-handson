42e9fce478e605441e3f73e5d266b4797fb40d22
TsukasaSekiguchi <t14159265358979323846@gmail.com>
2024-06-27 23:07:55 +0900

    微修正

--- script.ts
+++ script.ts
@@ -2,25 +2,14 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const createUser = async () => {
-    await prisma.user.create({
-        data: {
-            name: 'Alice',
-            email: 'test1@example.com',
-        },
-    })
-    console.log('User created!')
-    return
-}
-
 const users = async () => {
     const allUsers = await prisma.user.findMany()
     console.log(allUsers)
     return
 }
 
-// createUser()
-//     .catch((e) => {
-//         console.error(e)
-//     })
-users()
+const main = async () => {
+    await users()
+}
+
+main()
